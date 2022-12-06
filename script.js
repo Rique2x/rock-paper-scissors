34d2250b43ad10e1048f481a30f1a6d467b4c413
Rique2 <enrique2heinrich@gmail.com>
2022-12-06 15:11:42 +0200

    add some functions (script) p1

---
+++ script.js
@@ -0,0 +1,13 @@
+function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+}
+
+function playGame(){
+    const rockBtn = document.querySelector('rock');
+    const paperBtn = document.querySelector('paper');
+    const scissorBtn = document.querySelector ('scissor');
+    const playerOptions = ['rockBtn', 'paperBtn', 'scissorBtn'];
+    const computerOptions = ['rock', 'paper', 'scissor']
+
+}
